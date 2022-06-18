2eaacb47fbf981c4f200e7af5b09f6b1874844ed
Shedrack <shedrack@localhost.localdomain>
2022-06-18 03:27:23 +0100

    Fixes to the Die component

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 export default function Die(props) {
+    const style = {
+        background: props.isHeld ? "#59E391" : "white"
+    }
     return (
         <div
+            style={style}
             className="die"
             onClick={() => props.handleClick(props.id)}>
             {props.value}
